9926578f246d97910eeba8374684f9b15b709f85
Andy Brown <andyb5051@googlemail.com>
2013-11-10 01:15:00 +0000

    intial commit

--- Ola/OlaViewController.h
+++ Ola/OlaViewController.h
@@ -9,5 +9,7 @@
 #import <UIKit/UIKit.h>
 
 @interface OlaViewController : UIViewController
+- (IBAction)pushMe:(id)sender;
+@property (weak, nonatomic) IBOutlet UILabel *helloWorld;
 
 @end
--- Ola/OlaViewController.m
+++ Ola/OlaViewController.m
@@ -26,4 +26,7 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)pushMe:(id)sender {
+    _helloWorld.text = @"You pushed a button";
+}
 @end
